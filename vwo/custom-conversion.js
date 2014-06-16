d571f848be6b4453d16310d422aea8aafd30babe
Michael Kjeldsen <exp@firewerx.dk>
2014-06-16 20:36:06 +0200

    Create custom-conversion.js

---
+++ vwo/custom-conversion.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">// <![CDATA[
+if(typeof(_vis_opt_top_initialize) == "function") {
+	goalID = 202;
+	testID = 51;
+	// Code for Custom Goal: Goal #1
+	_vis_opt_goal_conversion(goalID);
+	// uncomment the following line to introduce a half second pause to ensure goal always registers with the server
+	// _vis_opt_pause(500);
+
+	window._vis_opt_queue = window._vis_opt_queue || [];
+	//Do change the Goal ID below AND the Test ID to the one shown in the Code snippet tab of the test.
+	window._vis_opt_queue.push(function() {_vis_opt_register_conversion(goalID, testID);});
+}
+// ]]></script>
