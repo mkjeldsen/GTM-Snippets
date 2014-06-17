e189247c7c9249907b4d54eaf3e42a9d2013b226
Michael Kjeldsen <exp@firewerx.dk>
2014-06-17 10:20:21 +0200

    Installationsnoter opdateret

--- README.md
+++ README.md
@@ -5,4 +5,12 @@ Kodestumper til Google Tag Manager
 
 ## VWO
 
-- custom-conversion.js virker pt ikke...
+### Custom Conversion installationsnoter
+
+1. Opret en regel, der udløses på kvitteringssiden (fx "konvertering").
+2. Opret et "Tilpasset HTML tag", og indsæt nedenstående javascript (inkl <script>-tags).
+ - Sæt udløsningsregel til at være din nyoprettede regel.
+3. Opret en Custom Conversion i Visual Website Optimizer.
+4. Find goalID'et for din conversion (ses i kodestumpen i VWO - fanen "Code Snippet") og opdatér variablen nedenfor.
+5. Find testID'et for din test (kan ses i URL'en når du kigger på den enkelte test i VWO's interface) og opdatér variablen nedenfor.
+6. Test og udgiv.
--- vwo/custom-conversion.js
+++ vwo/custom-conversion.js
@@ -1,3 +1,14 @@
+/*
+	1. Opret en regel, der udløses på kvitteringssiden (fx "konvertering").
+	2. Opret et "Tilpasset HTML tag", og indsæt nedenstående javascript (inkl <script>-tags).
+		2.1 Sæt udløsningsregel til at være din nyoprettede regel.
+	3. Opret en Custom Conversion i Visual Website Optimizer.
+	4. Find goalID'et for din conversion (ses i kodestumpen i VWO - fanen "Code Snippet") og opdatér variablen nedenfor.
+	5. Find testID'et for din test (kan ses i URL'en når du kigger på den enkelte test i VWO's interface) og opdatér variablen nedenfor.
+	6. Test og udgiv.
+*/
+
+
 <script type="text/javascript">// <![CDATA[
 if(typeof(_vis_opt_top_initialize) == "function") {
 	var goalID = 202;
