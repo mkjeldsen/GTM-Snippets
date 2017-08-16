f778e6b85821d71410af6fcef3d5cd53adca4e9d
Michael Kjeldsen <exp@firewerx.dk>
2017-08-16 09:46:55 +0200

    Added Danish CPR filter

--- misc/remove-pii-from-querystring.js
+++ misc/remove-pii-from-querystring.js
@@ -3,6 +3,9 @@ function(){
 	// Use variable to overwrite 'page' field i GA-tags
 	var queryString = location.search.substring(1);
 	queryString = queryString.length ? '&' + queryString : '';
-	var newQ = queryString.length ? '?' + queryString.replace(/&[^&@]+@[^&]+/g,'&emailRemoved').replace(/\b\d{13,16}\b/g,'&ccRemoved').substring(1) : '';
+	// US CC
+		//var newQ = queryString.length ? '?' + queryString.replace(/&[^&@]+@[^&]+/g,'&emailRemoved').replace(/\b\d{13,16}\b/g,'&ccRemoved').substring(1) : '';
+	// DK CPR
+		//var newQ = queryString.length ? '?' + queryString.replace(/&[^&@]+@[^&]+/g,'&emailRemoved').replace(/\b[1-3][\d](0[1-9]|1[0-2])[\d]{2}\-[\d]{4}\b/g,'&cprRemoved').substring(1) : '';
 	return newQ.length <= 1 ? '' : newQ;
 }
