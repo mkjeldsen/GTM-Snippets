bb4adaefea245d29d184c5b1b6d45c73bf42f9ba
Michael Kjeldsen <exp@firewerx.dk>
2017-10-19 09:39:13 +0200

    Create site-content-based-on-referrer.js

---
+++ misc/site-content-based-on-referrer.js
@@ -0,0 +1,9 @@
+var docRef = document.referrer;
+var refDomain = docRef.match(/:\/\/(.[^/]+/)[1];
+
+if (refDomain == 'chiliklaus.dk') {
+	// hvis du kommer fra chiliklaus.dk får du dette stærke indhold...
+}
+if (refDomain == 'marketingcamp.dk') {
+	// ;-)
+}
